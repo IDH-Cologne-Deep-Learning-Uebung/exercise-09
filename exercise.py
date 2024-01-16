60bed229a7c6e7357ba8fcf49a8123e59e536a39
WavyLotos <shohman4@smail.uni-koeln.de>
2024-01-16 11:16:20 +0100

    HA09

--- exercise.py
+++ exercise.py
@@ -1,32 +1,21 @@
 import pandas as pd
 import numpy as np
 
-# read in CSV file
 data = pd.read_csv("data/gmb.csv",encoding = 'latin1')
 
-# the first column of the file contains the sentence number
-# -- but only for the first token of each sentence.
-# The following line fills the rows downwards.
 data = data.fillna(method = 'ffill')
 
-# create a list of unique words and assign an integer number to it
 unique_words, coded_words = np.unique(data["Word"], return_inverse=True)
 data["Word_idx"] = coded_words
 EMPTY_WORD_IDX = len(unique_words)
 np.array(unique_words.tolist().append("_____"))
 num_words = len(unique_words)+1
 
-# create a list of unique tags and assign an integer number to it
 unique_tags, coded_tags = np.unique(data["Tag"], return_inverse=True)
 data["Tag_idx"]  = coded_tags
 NO_TAG_IDX = unique_tags.tolist().index("O")
 num_words_tag = len(unique_tags)
 
-# for verification and inspection, we can inspect the table so far
-# data[1:20]
-
-# We are interested in sentence-wise processing.
-# Therefore, we use a function that gives us individual sentences.
 def get_sentences(data):
   n_sent=1
   agg_func = lambda s:[(w,p,t)
@@ -42,26 +31,19 @@ sentences = get_sentences(data)
 from keras.utils import pad_sequences
 from keras.utils import to_categorical
 
-# find the maximum length for the sentences
 max_len = max([len(s) for s in sentences])
 
-# extract the word index
 x = np.array([ np.array([ w[0] for w in s ]) for s in sentences ])
-# extract the tag index
 y = np.array([ np.array([ w[2] for w in s ]) for s in sentences ])
 
-# shorter sentences are now padded to same length, using (index of) padding symbol
 x = pad_sequences(maxlen = max_len, sequences = x,
   padding = 'post', value = EMPTY_WORD_IDX)
 
-# we do the same for the y data
 y = pad_sequences(maxlen = max_len, sequences = y,
   padding = 'post', value = NO_TAG_IDX)
 
-# but we also convert the indices to one-hot-encoding
 y = np.array([to_categorical(i, num_classes = num_words_tag) for i in  y])
 
-# split the data into training and test data
 from sklearn.model_selection import train_test_split
 
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.1,random_state=1)
@@ -75,15 +57,24 @@ model.add(layers.LSTM(units = 5, return_sequences = True))
 model.add(layers.Dense(num_words_tag, activation = 'softmax'))
 model.summary()
 
-# We use a different optimizer this time
 model.compile(optimizer='Adam',
   loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
+from sklearn.utils.class_weight import compute_class_weight
+
+class_weights = compute_class_weight('balanced', np.unique(np.argmax(y_train, axis=2)), np.argmax(y_train, axis=2))
+
+sample_weights = np.zeros((y_train.shape[0], y_train.shape[1], num_words_tag))
+for i in range(y_train.shape[0]):
+    for j in range(y_train.shape[1]):
+        sample_weights[i, j, :] = class_weights[np.argmax(y_train[i, j, :])]
+
 history = model.fit(
     x_train, np.array(y_train),
-    batch_size = 64,
-    epochs = 1,
-    verbose = 1
+    batch_size=64,
+    epochs=1,
+    verbose=1,
+    sample_weight=sample_weights
 )
 
 model.evaluate(x_test, np.array(y_test))
